b3041314ea9c3bc678eaf9bad90e669b3ba1cf37
Josh Agar <jca318@lehigh.edu>
2023-01-05 09:52:18 -0500

    update fix

--- batch.sh
+++ batch.sh
@@ -1,5 +1,5 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11
 do
  echo Lecture_$1
- python create_user_script.py Lecture_$1 Lecture_$1 2 $1 $2
+ python create_user_script.py Lecture_{$1} Lecture_{$1} 2 $1 $2
 done
