6e0a74a03a1875008a941124b21e89aef88a33a5
Josh Agar <jca318@lehigh.edu>
2023-01-05 09:55:30 -0500

    full push

--- batch.sh
+++ batch.sh
@@ -1,5 +1,11 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11
 do
  echo Lecture_$1
- python create_user_script.py Lecture_${i} Lecture_${i} 2 $1 $2
+ python create_user_script.py Lecture_${i} Lecture_${i} 200 $1 $2
+done
+
+for i in 1 2 3 4 5 6 7 8 9 10 11
+do
+ echo Lab_$1
+ python create_user_script.py Lab_${i} Lab_${i} 200 $1 $2
 done
