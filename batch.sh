317271b80a0d66683ac96d44ec038432c2776d5c
Josh Agar <jca318@lehigh.edu>
2023-01-07 00:07:17 -0500

    fix case

--- batch.sh
+++ batch.sh
@@ -1,11 +1,11 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11
 do
- echo Lecture_$1
+ echo lecture_$1
  python create_user_script.py Lecture_${i} Lecture_${i} 200 $1 $2
 done
 
 for j in 1 2 3 4 5 6 7 8 9 10 11
 do
- echo Lab_$1
+ echo lab_$1
  python create_user_script.py Lab_${j} Lab_${j} 200 $1 $2
 done
