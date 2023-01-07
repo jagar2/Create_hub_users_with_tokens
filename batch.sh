5b7ad1462d6ba9e4078fa84879c2e5402a1ab173
Josh Agar <jca318@lehigh.edu>
2023-01-07 00:07:46 -0500

    fix case

--- batch.sh
+++ batch.sh
@@ -1,11 +1,11 @@
 for i in 1 2 3 4 5 6 7 8 9 10 11
 do
  echo lecture_$1
- python create_user_script.py Lecture_${i} Lecture_${i} 200 $1 $2
+ python create_user_script.py lecture_${i} lecture_${i} 200 $1 $2
 done
 
 for j in 1 2 3 4 5 6 7 8 9 10 11
 do
  echo lab_$1
- python create_user_script.py Lab_${j} Lab_${j} 200 $1 $2
+ python create_user_script.py lab_${j} lab_${j} 200 $1 $2
 done
